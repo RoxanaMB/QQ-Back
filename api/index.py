9e3403e1fcd8515e57682483bbe1350e52d3b4e4
RoxanaMB <alu0101339887@ull.edu.es>
2023-12-19 23:47:28 +0000

    a

---
+++ api/index.py
@@ -0,0 +1,44 @@
+import json
+import g4f
+import os
+
+from flask import Flask, Response, stream_with_context
+# from flask_socketio import SocketIO
+
+app = Flask(__name__)
+# socketio = SocketIO(app)
+
+port = int(os.environ.get("PORT", 8080))
+
+@app.route('/')
+def home():
+    # question = request.json['question']
+    # time.sleep(5)
+
+
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Hola, como estas?"}],
+        stream=True,
+    )
+    # sol = ""
+    # for message in response:
+    #     sol += message
+    #     print(message)
+        
+    def generate():
+        for part in response:
+            yield part.format(json.dumps(part))
+
+    # return Response(generate(), mimetype='text/event-stream')
+    return app.response_class(stream_with_context(generate()))
+        
+    # return sol
+
+
+@app.route('/about')
+def about():
+    return 'About'
+
+if __name__ == "__main__":
+    app.run(port=port)
