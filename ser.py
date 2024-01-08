e44ddb76e684879a06ac12e675aeec589c60a549
RoxanaMB <alu0101339887@ull.edu.es>
2024-01-08 16:14:59 +0000

    Prueba

--- ser.py
+++ ser.py
@@ -12,14 +12,33 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route('/', methods = ['POST', 'GET'])
 def home():
+    # Print all available providers
+    print([
+        provider.__name__
+        for provider in g4f.Provider.__providers__
+        if provider.working
+    ])
+
+    # Providers que funcionan:
+    #   - You
+    #   - AiChatOnline
+    #   - ChatBase (LENTO)
+    #   - ChatgptAi (LENTITO)
+    #   - ChatgptNext
+    #   - DeepInfra (MUY LENTO)
+    #   - GptGo (RÁPIDO)
+    #   - Koala
+    #   - Llama2 (g4f.models.llama2_70b)
+    #   - OnlineGpt
+
     if request.method == 'GET':
         return render_template('ind.html')
     if request.form['question']:
         question = 'Yo: ' + request.form['question']
 
         response = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            provider=g4f.Provider.Aichat,
+            model=g4f.models.llama2_70b,
+            provider=g4f.Provider.DeepInfra,
             messages=[
                 {"role": "user", "content": request.form['question']}
             ],
