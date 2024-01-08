f49748c1a3a17ed5e47ac10baaa26e5235b571ad
RoxanaMB <alu0101339887@ull.edu.es>
2024-01-08 14:22:49 +0000

    Prueba

---
+++ ser.py
@@ -0,0 +1,41 @@
+import os
+import g4f
+import json
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+conversations = []
+
+port = int(os.environ.get("PORT", 5000))
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def home():
+    if request.method == 'GET':
+        return render_template('ind.html')
+    if request.form['question']:
+        question = 'Yo: ' + request.form['question']
+
+        response = g4f.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": request.form['question']}
+            ],
+            stream=True,
+        )
+
+        answer = 'AI: '
+        for part in response:
+            answer += part.format(json.dumps(part))
+
+        conversations.append(question)
+        conversations.append(answer)
+
+        return render_template('ind.html', chat = conversations)
+    else:
+        return render_template('ind.html')
+
+
+if __name__ == "__main__":
+    app.run(port=port)
--- server.py
+++ server.py
@@ -2,46 +2,29 @@ import json
 import g4f
 import os
 
-from flask import Flask, Response, stream_with_context
-# from flask_socketio import SocketIO
+from flask import Flask, stream_with_context
 
 app = Flask(__name__)
-# socketio = SocketIO(app)
 
 port = int(os.environ.get("PORT", 8080))
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    # question = request.json['question']
-    # time.sleep(5)
-
-
     response = g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": "Que pasó en la primera querra mundial?"}],
+        messages=[
+            {"role": "user", "content": "Que pasó en la primera querra mundial?"}
+        ],
         stream=True,
     )
-    # sol = ""
-    # for message in response:
-    #     sol += message
-    #     print(message)
-        
+
     def generate():
-        sol = ""
         for part in response:
-            sol += part
-            print(part)
             yield part.format(json.dumps(part))
 
-    # return Response(generate(), mimetype='text/event-stream')
     return app.response_class(stream_with_context(generate()))
-        
-    # return sol
 
-
-@app.route('/about')
-def about():
-    return 'About'
 
 if __name__ == "__main__":
     app.run(port=port)
