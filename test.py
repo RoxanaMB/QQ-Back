e4ea7932a275bbae584868456dd9725008766c95
RoxanaMB <alu0101339887@ull.edu.es>
2024-01-10 20:42:43 +0000

    Add Flask app with user registration, login, and CRUD operations

---
+++ test.py
@@ -0,0 +1,18 @@
+from db import supabase
+
+email: str = 'roxanababa5@gmail.com'
+password: str = 'roxanababa18'
+
+user = supabase.auth.sign_up({
+  "email": email, 
+  "password": password,
+  "options": {
+    "data": {
+      "username": "roxanababa5"
+    }
+  }
+})
+
+
+print(user.user.id)
+print(user.session.access_token)
