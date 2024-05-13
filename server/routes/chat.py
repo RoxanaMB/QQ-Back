af877b886f2de9cc6954a41a927efb4c06060c3c
RoxanaMB <alu0101339887@ull.edu.es>
2024-05-13 20:47:03 +0100

    Refactor chat.py: Remove commented code and sort messages by date

--- server/routes/chat.py
+++ server/routes/chat.py
@@ -21,31 +21,13 @@ def create_chat():
     return jsonify(res.data)
 
 
-# # Cambiar modelo de un chat
-# @chat.route('/chat/<int:id>', methods=['PUT'])
-# def update_chat_model(id):
-#     # Obtener datos del chat
-#     data = request.get_json()
-
-#     # Actualizar chat
-#     res = supabase.table('chats').update({
-#         "model": data['model'],
-#     }).eq('id', id).execute()
-#     return jsonify(res.data)
-
-# return this.http.get(`http://127.0.0.1:5000/chat/${chat_id}/messages`);
-
 # Obtener mensajes de un chat según su id
 @chat.route('/chat/<id>/messages', methods=['GET'])
 def get_chat_messages(id):
     # Obtener mensajes
     messages = supabase.table('messages').select('*').eq('chat', id).execute()
 
-    ## Añadir role = user si model == NULL o role = assistant si model != NULL
-    for message in messages.data:
-        if message['model'] == None:
-            message['role'] = 'user'
-        else:
-            message['role'] = 'assistant'
+    # Ordenar mensajes por fecha
+    messages.data.sort(key=lambda x: x['created_at'])
             
     return jsonify(messages.data)
